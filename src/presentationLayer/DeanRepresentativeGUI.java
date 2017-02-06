7c3b73f2ecb77922cf5d65316fbfda5e5e6cede4
Igor Danielewicz <igor.danielewicz@gmail.com>
2017-02-06 21:38:56 +0100

    DR - ide zryc 	modified:   ThesisatorExtra.bak 	modified:   src/presentationLayer/DeanRepresentativeGUI.java

--- src/presentationLayer/DeanRepresentativeGUI.java
+++ src/presentationLayer/DeanRepresentativeGUI.java
@@ -3,6 +3,8 @@ package presentationLayer;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
@@ -21,6 +23,12 @@ import java.awt.Dimension;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.MatteBorder;
 import javax.swing.table.DefaultTableModel;
+
+import buisnessLayer.Teacher;
+import buisnessLayer.Thesis;
+import buisnessLayer.Topic;
+import buisnessLayer.User;
+
 import javax.swing.ImageIcon;
 import java.awt.CardLayout;
 import javax.swing.JButton;
@@ -33,6 +41,7 @@ public class DeanRepresentativeGUI {
 	private JTable table;
 	private JTable tableAssignReviewersTheses;
 	private JTable tableAssignReviewersTeachers;
+	private User user;
 
 	/**
 	 * Launch the application.
@@ -41,7 +50,7 @@ public class DeanRepresentativeGUI {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					DeanRepresentativeGUI window = new DeanRepresentativeGUI();
+					DeanRepresentativeGUI window = new DeanRepresentativeGUI(args);
 					window.frmThesisatorextra.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -53,14 +62,16 @@ public class DeanRepresentativeGUI {
 	/**
 	 * Create the application.
 	 */
-	public DeanRepresentativeGUI() {
+	public DeanRepresentativeGUI(String[] args) throws Exception {
+		user = new User(args[0],args[1],Integer.parseInt(args[2]),args[3],args[4],Integer.parseInt(args[5]));
 		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
+	 * @throws Exception 
 	 */
-	private void initialize() {
+	private void initialize() throws Exception {
 		frmThesisatorextra = new JFrame();
 		frmThesisatorextra.setResizable(false);
 		frmThesisatorextra.setTitle("THESISATOR-EXTRA");
@@ -82,13 +93,17 @@ public class DeanRepresentativeGUI {
 		scrollPane.setPreferredSize(new Dimension(750, 400));
 		panelThesisTopics.add(scrollPane);
 		
+		List<Topic> listTopics = user.getServer().getAvailableTopics(user.getDepartment());
+		ArrayList<String[]> list = new ArrayList<String[]>();
+		for(Topic topic : listTopics){
+			String[] row = {topic.getTopic(),topic.getSupervisor()};
+			list.add(row);
+		}
+		
 		table = new JTable();
 		scrollPane.setViewportView(table);
 		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{"adsdad asdasd aasd asdasd asd asd", "g sdfg sdfg d"},
-				{"sa dasd asda d dasda sfgdfgdfsg sdfg sdfgs dfgdfg sdg", "gdfs gdsfgs"},
-			},
+				list.toArray(new String[list.size()][]),
 			new String[] {
 				"Thesis topic", "Teacher name"
 			}
@@ -119,11 +134,17 @@ public class DeanRepresentativeGUI {
 		panelAssignReviewersTheses.add(scrollPaneAssignReviewersTheses);
 		scrollPaneAssignReviewersTheses.setPreferredSize(new Dimension(700, 400));
 		
+		List<Thesis> listTheses = user.getServer().getThesesToAssignReviewers(user.getDepartment());
+		ArrayList<String[]> listToAssign = new ArrayList<String[]>();
+		for(Thesis thesis : listTheses){
+			String action = thesis.getReviewerName() == null ? "Add Reviewer" : "Change";
+			String[] row = {thesis.getThesisName(),thesis.getSupervisorName(),thesis.getReviewerName(), action};
+			listToAssign.add(row);
+		}
+		
 		tableAssignReviewersTheses = new JTable();
 		tableAssignReviewersTheses.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null, "Add Reviewer"},
-			},
+				listToAssign.toArray(new String[listToAssign.size()][]),
 			new String[] {
 				"Topic", "Supervisor", "Reviewer", "Action"
 			}
@@ -149,12 +170,37 @@ public class DeanRepresentativeGUI {
 		tableAssignReviewersTheses.getColumn("Action").setCellEditor(new JButtonEditor(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            	CardLayout cardLayout = (CardLayout) panelAssignReviewers.getLayout();
-            	cardLayout.show(panelAssignReviewers, "Teachers");
-            	//JRowButton button = (JRowButton)e.getSource();
-            	//String thesis = (String) tableAssignReviewersTheses.getModel().getValueAt(button.getRow(), 0);
-            	//String name = (String) tableAssignReviewersTheses.getModel().getValueAt(button.getRow(), 1);
-            	//JOptionPane.showMessageDialog(null, new String("Topic: " + thesis + "\nName: " + name));
+            	try {
+	            	CardLayout cardLayout = (CardLayout) panelAssignReviewers.getLayout();
+	            	cardLayout.show(panelAssignReviewers, "Teachers");
+	            	JRowButton button = (JRowButton)e.getSource();
+					List<Teacher> listTeachers = user.getServer().getTeacherList(listTheses.get(button.getRow()));
+	        		ArrayList<String[]> listToAssign = new ArrayList<String[]>();
+	        		for(Teacher teacher : listTeachers){
+	        			String[] row = {teacher.getName(),teacher.getDegree(),"Assign"};
+	        			listToAssign.add(row);
+	        		}
+	            	tableAssignReviewersTeachers.setModel(new DefaultTableModel(
+	            			listToAssign.toArray(new String[listToAssign.size()][]),
+	        			new String[] {
+	        				"Name", "Degree","Assign"
+	        			}
+	        		) {
+	        			boolean[] columnEditables = new boolean[] {
+	        				false, false, true
+	        			};
+	        			public boolean isCellEditable(int row, int column) {
+	        				return columnEditables[column];
+	        			}
+	        		});
+	        		tableAssignReviewersTeachers.getColumnModel().getColumn(0).setResizable(false);
+	        		tableAssignReviewersTeachers.getColumnModel().getColumn(1).setResizable(false);
+	        		tableAssignReviewersTeachers.getColumnModel().getColumn(2).setResizable(false);
+            
+	            } catch (Exception e1) {
+	            	JOptionPane.showMessageDialog(null, "Connection error!");
+	            	e1.printStackTrace();
+	            }
             }
         }));
 		
@@ -172,14 +218,14 @@ public class DeanRepresentativeGUI {
 		tableAssignReviewersTeachers = new JTable();
 		tableAssignReviewersTeachers.setModel(new DefaultTableModel(
 			new Object[][] {
-				{null, null, null, null, "Assign"},
+				{null, null, "Assign"},
 			},
 			new String[] {
-				"Name", "Degree", "Specialization", "Assigned thesis", "Assign"
+				"Name", "Degree", "Assign"
 			}
 		) {
 			boolean[] columnEditables = new boolean[] {
-				false, false, false, false, false
+				false, false, true
 			};
 			public boolean isCellEditable(int row, int column) {
 				return columnEditables[column];
@@ -188,8 +234,6 @@ public class DeanRepresentativeGUI {
 		tableAssignReviewersTeachers.getColumnModel().getColumn(0).setResizable(false);
 		tableAssignReviewersTeachers.getColumnModel().getColumn(1).setResizable(false);
 		tableAssignReviewersTeachers.getColumnModel().getColumn(2).setResizable(false);
-		tableAssignReviewersTeachers.getColumnModel().getColumn(3).setResizable(false);
-		tableAssignReviewersTeachers.getColumnModel().getColumn(4).setResizable(false);
 		scrollPaneAssignReviewersTeachers.setViewportView(tableAssignReviewersTeachers);
 		
 		JButton btnBack = new JButton("Back");
@@ -208,6 +252,8 @@ public class DeanRepresentativeGUI {
             	JRowButton button = (JRowButton)e.getSource();
             	String thesis = (String) tableAssignReviewersTeachers.getModel().getValueAt(button.getRow(), 0);
             	String name = (String) tableAssignReviewersTeachers.getModel().getValueAt(button.getRow(), 1);
+            	//user.getServer().assignReviewer(thesis, teacher);
+            	//user.getServer().
             	JOptionPane.showMessageDialog(null, new String("Topic: " + thesis + "\nName: " + name));
             }
         }));
